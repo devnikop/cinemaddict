015d776d5163bb52641e4772bfedc452142d1283
devnikop <petrov.petrov.ak@gmail.com>
2019-03-02 14:59:14 +0300

    add click handler for filter elements

--- src/main.js
+++ src/main.js
@@ -2,42 +2,53 @@ import {filterRender} from './make-filter.js';
 import {getRandomInt} from './util.js';
 import {filmCardRender} from './make-film-card.js';
 
-const FILM_COUNT_MIN = 2;
-const FILM_COUNT_MAX = 5;
+const FILM_COUNT_MIN = 1;
+const FILM_COUNT_MAX = 7;
 const FILM_CARDS_COUNT = 7;
-{
-  const mainNavigationContainerElement = document.querySelector(`.main-navigation`);
-  let tempFilterContainer = ``;
-  tempFilterContainer += filterRender(`All movies`);
-  tempFilterContainer += filterRender(`Watchlist`, getRandomInt(FILM_COUNT_MIN, FILM_COUNT_MAX));
-  tempFilterContainer += filterRender(`History`, getRandomInt(FILM_COUNT_MIN, FILM_COUNT_MAX));
-  tempFilterContainer += filterRender(`Favorites`, getRandomInt(FILM_COUNT_MIN, FILM_COUNT_MAX));
-  mainNavigationContainerElement.insertAdjacentHTML(`afterbegin`, tempFilterContainer);
-}
 
-{
-  const filmsListContainerElement = document.querySelector(`.films-list .films-list__container`);
+const filterClickHandler = (evt) => {
+  evt.preventDefault();
+  const filmCardElements = filmsListContainerElement.querySelectorAll(`.film-card `);
+  for (const element of filmCardElements) {
+    element.remove();
+  }
   let tempFilmsListContainer = ``;
-  for (let i = 0; i < FILM_CARDS_COUNT; i++) {
+  for (let i = 0; i < getRandomInt(FILM_COUNT_MIN, FILM_COUNT_MAX); i++) {
     tempFilmsListContainer += filmCardRender();
   }
   filmsListContainerElement.insertAdjacentHTML(`beforeend`, tempFilmsListContainer);
+};
+
+const mainNavigationContainerElement = document.querySelector(`.main-navigation`);
+let tempFilterContainer = ``;
+tempFilterContainer += filterRender(`All movies`);
+tempFilterContainer += filterRender(`Watchlist`, getRandomInt(FILM_COUNT_MIN, FILM_COUNT_MAX));
+tempFilterContainer += filterRender(`History`, getRandomInt(FILM_COUNT_MIN, FILM_COUNT_MAX));
+tempFilterContainer += filterRender(`Favorites`, getRandomInt(FILM_COUNT_MIN, FILM_COUNT_MAX));
+mainNavigationContainerElement.insertAdjacentHTML(`afterbegin`, tempFilterContainer);
+
+const filterElements = mainNavigationContainerElement.querySelectorAll(`.make-navigation__item--js`);
+for (const element of filterElements) {
+  element.addEventListener(`click`, filterClickHandler);
 }
 
-{
-  const topRatedContainerElement = document.querySelector(`.films-list__container--top-rated`);
-  let tempTopRatedContainer = ``;
-  for (let i = 0; i < 2; i++) {
-    tempTopRatedContainer += filmCardRender(true);
-  }
-  topRatedContainerElement.insertAdjacentHTML(`beforeend`, tempTopRatedContainer);
+const filmsListContainerElement = document.querySelector(`.films-list .films-list__container`);
+let tempFilmsListContainer = ``;
+for (let i = 0; i < FILM_CARDS_COUNT; i++) {
+  tempFilmsListContainer += filmCardRender();
 }
+filmsListContainerElement.insertAdjacentHTML(`beforeend`, tempFilmsListContainer);
 
-{
-  const mostCommentedContainerElement = document.querySelector(`.films-list__container--most-commented`);
-  let tempMostCommentedContainer = ``;
-  for (let i = 0; i < 2; i++) {
-    tempMostCommentedContainer += filmCardRender(true);
-  }
-  mostCommentedContainerElement.insertAdjacentHTML(`beforeend`, tempMostCommentedContainer);
+const topRatedContainerElement = document.querySelector(`.films-list__container--top-rated`);
+let tempTopRatedContainer = ``;
+for (let i = 0; i < 2; i++) {
+  tempTopRatedContainer += filmCardRender(true);
 }
+topRatedContainerElement.insertAdjacentHTML(`beforeend`, tempTopRatedContainer);
+
+const mostCommentedContainerElement = document.querySelector(`.films-list__container--most-commented`);
+let tempMostCommentedContainer = ``;
+for (let i = 0; i < 2; i++) {
+  tempMostCommentedContainer += filmCardRender(true);
+}
+mostCommentedContainerElement.insertAdjacentHTML(`beforeend`, tempMostCommentedContainer);
--- src/make-filter.js
+++ src/make-filter.js
@@ -1,4 +1,4 @@
 export const filterRender = (filterName, filmCount = 0) =>
-  `<a href="#${filterName}" class="main-navigation__item">${filterName}
+  `<a href="#${filterName}" class="main-navigation__item make-navigation__item--js">${filterName}
     ${filmCount === 0 ? `` : `<span class="main-navigation__item-count">${filmCount}</span>`}
   </a>`;
