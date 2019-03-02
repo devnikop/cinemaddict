1bd3df74110ddcf7f4f3f6cd43beeeb9f4620ce1
devnikop <petrov.petrov.ak@gmail.com>
2019-03-02 14:25:20 +0300

    create make-film-card.js module

--- src/main.js
+++ src/main.js
@@ -1,14 +1,43 @@
 import {filterRender} from './make-filter.js';
 import {getRandomInt} from './util.js';
+import {filmCardRender} from './make-film-card.js';
 
 const FILM_COUNT_MIN = 2;
 const FILM_COUNT_MAX = 5;
+const FILM_CARDS_COUNT = 7;
+{
+  const mainNavigationContainerElement = document.querySelector(`.main-navigation`);
+  let tempFilterContainer = ``;
+  tempFilterContainer += filterRender(`All movies`);
+  tempFilterContainer += filterRender(`Watchlist`, getRandomInt(FILM_COUNT_MIN, FILM_COUNT_MAX));
+  tempFilterContainer += filterRender(`History`, getRandomInt(FILM_COUNT_MIN, FILM_COUNT_MAX));
+  tempFilterContainer += filterRender(`Favorites`, getRandomInt(FILM_COUNT_MIN, FILM_COUNT_MAX));
+  mainNavigationContainerElement.insertAdjacentHTML(`afterbegin`, tempFilterContainer);
+}
 
-const mainNavigationContainerElement = document.querySelector(`.main-navigation`);
-let tempFilterContainer = ``;
-tempFilterContainer += filterRender(`All movies`);
-tempFilterContainer += filterRender(`Watchlist`, getRandomInt(FILM_COUNT_MIN, FILM_COUNT_MAX));
-tempFilterContainer += filterRender(`History`, getRandomInt(FILM_COUNT_MIN, FILM_COUNT_MAX));
-tempFilterContainer += filterRender(`Favorites`, getRandomInt(FILM_COUNT_MIN, FILM_COUNT_MAX));
+{
+  const filmsListContainerElement = document.querySelector(`.films-list .films-list__container`);
+  let tempFilmsListContainer = ``;
+  for (let i = 0; i < FILM_CARDS_COUNT; i++) {
+    tempFilmsListContainer += filmCardRender();
+  }
+  filmsListContainerElement.insertAdjacentHTML(`beforeend`, tempFilmsListContainer);
+}
 
-mainNavigationContainerElement.insertAdjacentHTML(`afterbegin`, tempFilterContainer);
+{
+  const topRatedContainerElement = document.querySelector(`.films-list__container--top-rated`);
+  let tempTopRatedContainer = ``;
+  for (let i = 0; i < 2; i++) {
+    tempTopRatedContainer += filmCardRender(true);
+  }
+  topRatedContainerElement.insertAdjacentHTML(`beforeend`, tempTopRatedContainer);
+}
+
+{
+  const mostCommentedContainerElement = document.querySelector(`.films-list__container--most-commented`);
+  let tempMostCommentedContainer = ``;
+  for (let i = 0; i < 2; i++) {
+    tempMostCommentedContainer += filmCardRender(true);
+  }
+  mostCommentedContainerElement.insertAdjacentHTML(`beforeend`, tempMostCommentedContainer);
+}
--- src/make-filter.js
+++ src/make-filter.js
@@ -1,7 +1,4 @@
-export const filterRender = (filterName, filmCount = 0) => {
-  return `
-    <a href="#${filterName}" class="main-navigation__item">${filterName}
-      ${filmCount === 0 ? `` : `<span class="main-navigation__item-count">${filmCount}</span>`}
-    </a>
-  `;
-};
+export const filterRender = (filterName, filmCount = 0) =>
+  `<a href="#${filterName}" class="main-navigation__item">${filterName}
+    ${filmCount === 0 ? `` : `<span class="main-navigation__item-count">${filmCount}</span>`}
+  </a>`;
