743da081921517208062fe2150bc385e09d6c982
devnikop <petrov.petrov.ak@gmail.com>
2019-03-07 12:45:03 +0300

    divide the 'main.js' into functions

--- src/main.js
+++ src/main.js
@@ -9,20 +9,29 @@ const FILM_CARDS_COUNT = 7;
 const TOP_RATED_FILM_COUNT = 2;
 const MOST_COMMENTED_FILM_COUNT = 2;
 
-let filmCardList = [];
-for (let i = 0; i < FILM_CARDS_COUNT; i++) {
-  filmCardList[i] = renderFilmCard(filmCard);
-}
+const createFilmCardList = () => {
+  let filmCardList = [];
+  for (let i = 0; i < FILM_CARDS_COUNT; i++) {
+    filmCardList[i] = renderFilmCard(filmCard);
+  }
+  return filmCardList;
+};
 
-let topRatedFilmList = [];
-for (let i = 0; i < TOP_RATED_FILM_COUNT; i++) {
-  topRatedFilmList[i] = renderFilmCard(filmCard, true);
-}
+const createTopRatedFilmList = () => {
+  let topRatedFilmList = [];
+  for (let i = 0; i < TOP_RATED_FILM_COUNT; i++) {
+    topRatedFilmList[i] = renderFilmCard(filmCard, true);
+  }
+  return topRatedFilmList;
+};
 
-let mostCommentedFilmList = [];
-for (let i = 0; i < MOST_COMMENTED_FILM_COUNT; i++) {
-  mostCommentedFilmList[i] = renderFilmCard(filmCard, true);
-}
+const createMostCommentedFilmList = () => {
+  let mostCommentedFilmList = [];
+  for (let i = 0; i < MOST_COMMENTED_FILM_COUNT; i++) {
+    mostCommentedFilmList[i] = renderFilmCard(filmCard, true);
+  }
+  return mostCommentedFilmList;
+};
 
 const filterClickHandler = (evt) => {
   if (evt.target.matches(`.make-navigation__item--js`)) {
@@ -39,32 +48,27 @@ const filterClickHandler = (evt) => {
   }
 };
 
-const mainNavigationContainerElement = document.querySelector(`.main-navigation`);
-let tempFilterContainer = ``;
-tempFilterContainer += renderFilter(`All movies`);
-tempFilterContainer += renderFilter(`Watchlist`, getRandomInt(FILM_COUNT_MIN, FILM_COUNT_MAX));
-tempFilterContainer += renderFilter(`History`, getRandomInt(FILM_COUNT_MIN, FILM_COUNT_MAX));
-tempFilterContainer += renderFilter(`Favorites`, getRandomInt(FILM_COUNT_MIN, FILM_COUNT_MAX));
-mainNavigationContainerElement.insertAdjacentHTML(`afterbegin`, tempFilterContainer);
-mainNavigationContainerElement.addEventListener(`click`, filterClickHandler);
+const renderFilterList = () => {
+  const mainNavigationContainerElement = document.querySelector(`.main-navigation`);
+  let tempFilterContainer = ``;
+  tempFilterContainer += renderFilter(`All movies`);
+  tempFilterContainer += renderFilter(`Watchlist`, getRandomInt(FILM_COUNT_MIN, FILM_COUNT_MAX));
+  tempFilterContainer += renderFilter(`History`, getRandomInt(FILM_COUNT_MIN, FILM_COUNT_MAX));
+  tempFilterContainer += renderFilter(`Favorites`, getRandomInt(FILM_COUNT_MIN, FILM_COUNT_MAX));
+  mainNavigationContainerElement.insertAdjacentHTML(`afterbegin`, tempFilterContainer);
+  mainNavigationContainerElement.addEventListener(`click`, filterClickHandler);
+};
+
+const filmCardList = createFilmCardList();
+const topRatedFilmList = createTopRatedFilmList();
+const mostCommentedFilmList = createMostCommentedFilmList();
 
 const filmsListContainerElement = document.querySelector(`.films-list .films-list__container`);
-let tempFilmsListContainer = ``;
-filmCardList.forEach((currentFilmCard) => {
-  tempFilmsListContainer += currentFilmCard;
-});
-filmsListContainerElement.insertAdjacentHTML(`beforeend`, tempFilmsListContainer);
-
 const topRatedContainerElement = document.querySelector(`.films-list__container--top-rated`);
-let tempTopRatedContainer = ``;
-for (let i = 0; i < TOP_RATED_FILM_COUNT; i++) {
-  tempTopRatedContainer += topRatedFilmList[i];
-}
-topRatedContainerElement.insertAdjacentHTML(`beforeend`, tempTopRatedContainer);
-
 const mostCommentedContainerElement = document.querySelector(`.films-list__container--most-commented`);
-let tempMostCommentedContainer = ``;
-for (let i = 0; i < MOST_COMMENTED_FILM_COUNT; i++) {
-  tempMostCommentedContainer += mostCommentedFilmList[i];
-}
-mostCommentedContainerElement.insertAdjacentHTML(`beforeend`, tempMostCommentedContainer);
+
+filmsListContainerElement.insertAdjacentHTML(`beforeend`, filmCardList.join(``));
+topRatedContainerElement.insertAdjacentHTML(`beforeend`, topRatedFilmList.join(``));
+mostCommentedContainerElement.insertAdjacentHTML(`beforeend`, mostCommentedFilmList.join(``));
+
+renderFilterList();
