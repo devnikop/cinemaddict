3c26d1f1dfaa8e089ff5d0d8503d466a684aae62
devnikop <petrov.petrov.ak@gmail.com>
2019-03-13 11:59:39 +0300

    add function 'createElement' in util.js

--- src/util.js
+++ src/util.js
@@ -1,1 +1,7 @@
 export const getRandomInt = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
+
+export const createElement = (template) => {
+  const container = document.createElement(`div`);
+  container.insertAdjacentHTML(`afterbegin`, template);
+  return container.firstChild;
+};
