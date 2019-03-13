54a4bd4585740ea156f0fb593e668fc9c90ebece
devnikop <petrov.petrov.ak@gmail.com>
2019-03-13 11:58:51 +0300

    add class 'FilmCard'

---
+++ src/film-card.js
@@ -0,0 +1,53 @@
+import {createElement} from './util.js';
+
+export default class FilmCard {
+  constructor(film, cardControls = false) {
+    this._title = film.title;
+    this._rating = film.rating;
+    this._year = film.year;
+    this._duration = film.duration;
+    this._genre = film.genre;
+    this._poster = film.poster;
+    this._description = film.description;
+    this._commentsCount = film.commentsCount;
+    this._isOnWatchlist = film.isOnWatchlist;
+    this._isWatched = film.isWatched;
+    this._isFavorite = film.isFavorite;
+    this._cardControls = cardControls;
+
+    this._element = null;
+  }
+
+  get template() {
+    return `
+    <article class="film-card ${this._cardControls ? `film-card--no-controls` : ``}">
+      <h3 class="film-card__title">${this._title}</h3>
+      <p class="film-card__rating">${this._rating}</p>
+      <p class="film-card__info">
+        <span class="film-card__year">${this._year}</span>
+        <span class="film-card__duration">${this._duration}</span>
+        <span class="film-card__genre">${this._genre}</span>
+      </p>
+      <img src="${this._poster}" alt="" class="film-card__poster">
+      <p class="film-card__description">${this._description}</p>
+      <button class="film-card__comments">${this._commentsCount} comments</button>
+
+      ${this._cardControls ? `` : `
+        <form class="film-card__controls">
+          <button class="film-card__controls-item button film-card__controls-item--add-to-watchlist"><!--Add to watchlist--> WL</button>
+          <button class="film-card__controls-item button film-card__controls-item--mark-as-watched"><!--Mark as watched-->WTCHD</button>
+          <button class="film-card__controls-item button film-card__controls-item--favorite"><!--Mark as favorite-->FAV</button>
+        </form>`}
+    </article>`.trim();
+  }
+
+  bind() {
+
+  }
+
+  render() {
+    this._element = createElement(this.template);
+    this.bind();
+    return this._element;
+  }
+}
