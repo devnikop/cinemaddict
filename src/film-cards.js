07d8c58967e92ecb5e8960d2b113d6b2ff792456
devnikop <petrov.petrov.ak@gmail.com>
2019-04-03 18:39:37 +0300

    add feedback for UI

--- src/film-cards.js
+++ src/film-cards.js
@@ -2,31 +2,42 @@ import FilmCard from './film-card';
 import FilmDetails from './film-details';
 import _ from '../node_modules/lodash';
 
-// const blockForm = (editTaskComponent) => {
-//   cardFormElement = editTaskComponent.element.querySelector(`.card__inner`);
-//   cardFormElement.style.border = `1px solid black`;
-//   cardFormElement.classList.remove(`shake`);
-//   saveButtonElement = editTaskComponent.element.querySelector(`.card__save`);
-//   deleteButtonElement = editTaskComponent.element.querySelector(`.card__delete`);
-//   saveButtonElement.disabled = true;
-//   deleteButtonElement.disabled = true;
-// };
-
-// const unblockForm = () => {
-//   cardFormElement.classList.add(`shake`);
-//   cardFormElement.style.border = `1px solid red`;
-//   saveButtonElement.disabled = false;
-//   deleteButtonElement.disabled = false;
-// };
-
-// let cardFormElement = ``;
-// let saveButtonElement = ``;
-// let deleteButtonElement = ``;
-
 export default class FilmCards {
   constructor(api) {
     this._api = api;
+
+    this._commentInputElement = ``;
+    this._ratingsContainerElement = ``;
+    this._ratingInputElements = ``;
   }
+
+  _blockForm(filmDetails) {
+    this._commentInputElement = filmDetails.element.querySelector(`.film-details__comment-input`);
+    this._commentInputElement.style.border = `1px solid black`;
+    this._commentInputElement.classList.remove(`shake`);
+    this._commentInputElement.disabled = true;
+
+    this._ratingsContainerElement = filmDetails.element.querySelector(`.film-details__user-rating-score`);
+    this._ratingsContainerElement.style.border = `1px solid transparent`;
+    this._ratingsContainerElement.classList.remove(`shake`);
+    this._ratingInputElements = filmDetails.element.querySelectorAll(`.film-details__user-rating-label`);
+    for (const element of this._ratingInputElements) {
+      element.style.pointerEvents = `none`;
+    }
+  }
+
+  _unblockForm() {
+    this._commentInputElement.disabled = false;
+    this._commentInputElement.classList.add(`shake`);
+    this._commentInputElement.style.border = `1px solid red`;
+
+    this._ratingsContainerElement.classList.add(`shake`);
+    this._ratingsContainerElement.style.border = `1px solid red`;
+    for (const element of this._ratingInputElements) {
+      element.style.pointerEvents = `auto`;
+    }
+  }
+
   _bindHandlers(filmDetails, film, currentFilmCardData) {
     const _updateDetails = (newData) => {
       Object.assign(currentFilmCardData, newData);
@@ -36,6 +47,17 @@ export default class FilmCards {
           document.body.replaceChild(filmDetails.render(), currentFilmDetails);
           const updatedCurrentData = Object.assign(currentFilmCardData, newCard);
           film.update(_.cloneDeep(updatedCurrentData));
+        })
+        .then(() => {
+          if (this._commentInputElement) {
+            this._commentInputElement.disabled = false;
+          }
+          if (this._commentInputElement) {
+            this._commentInputElement.disabled = false;
+          }
+        })
+        .catch(() => {
+          this._unblockForm();
         });
     };
 
@@ -46,9 +68,11 @@ export default class FilmCards {
       document.querySelector(`.films-list__container`).replaceChild(film.render(), currentFilmCard);
     };
     filmDetails.onCommentEnter = (newData) => {
+      this._blockForm(filmDetails);
       _updateDetails(newData);
     };
     filmDetails.onUserRatingClick = (newData) => {
+      this._blockForm(filmDetails);
       _updateDetails(newData);
     };
     filmDetails.onAddToWatchList = (newState) => {
