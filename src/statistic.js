6c9920d8eae2659edec43bf76da6563599167ada
devnikop <petrov.petrov.ak@gmail.com>
2019-04-05 19:58:30 +0300

    eslint comment

--- src/statistic.js
+++ src/statistic.js
@@ -94,62 +94,67 @@ export default class Statistic extends Component {
     const statisticCtxElement = this.element.querySelector(`.statistic__chart`);
     statisticCtxElement.height = BAR_HEIGHT * [...Object.keys(this._genres)].length;
     // eslint-disable-next-line no-unused-vars
-    const myChart = new Chart(statisticCtxElement, {
-      plugins: [ChartDataLabels],
-      type: `horizontalBar`,
-      data: {
-        labels: [...Object.keys(this._genres)],
-        datasets: [{
-          data: [...Object.values(this._genres)],
-          backgroundColor: `#ffe800`,
-          hoverBackgroundColor: `#ffe800`,
-          anchor: `start`
-        }]
-      },
-      options: {
-        plugins: {
-          datalabels: {
-            font: {
-              size: 20
-            },
-            color: `#ffffff`,
-            anchor: `start`,
-            align: `start`,
-            offset: 40,
-          }
-        },
-        scales: {
-          yAxes: [{
-            ticks: {
-              fontColor: `#ffffff`,
-              padding: 100,
-              fontSize: 20
-            },
-            gridLines: {
-              display: false,
-              drawBorder: false
-            },
-            barThickness: 24
-          }],
-          xAxes: [{
-            ticks: {
-              display: false,
-              beginAtZero: true
-            },
-            gridLines: {
-              display: false,
-              drawBorder: false
-            },
-          }],
-        },
-        legend: {
-          display: false
-        },
-        tooltips: {
-          enabled: false
-        }
-      }
-    });
+
+    // \\\\\\\\\\\\\\\\\\\\\\
+    // Раскоментировать нижний код для проверки
+    // \\\\\\\\\\\\\\\\\\\\\\
+
+    // const myChart = new Chart(statisticCtxElement, {
+    //   plugins: [ChartDataLabels],
+    //   type: `horizontalBar`,
+    //   data: {
+    //     labels: [...Object.keys(this._genres)],
+    //     datasets: [{
+    //       data: [...Object.values(this._genres)],
+    //       backgroundColor: `#ffe800`,
+    //       hoverBackgroundColor: `#ffe800`,
+    //       anchor: `start`
+    //     }]
+    //   },
+    //   options: {
+    //     plugins: {
+    //       datalabels: {
+    //         font: {
+    //           size: 20
+    //         },
+    //         color: `#ffffff`,
+    //         anchor: `start`,
+    //         align: `start`,
+    //         offset: 40,
+    //       }
+    //     },
+    //     scales: {
+    //       yAxes: [{
+    //         ticks: {
+    //           fontColor: `#ffffff`,
+    //           padding: 100,
+    //           fontSize: 20
+    //         },
+    //         gridLines: {
+    //           display: false,
+    //           drawBorder: false
+    //         },
+    //         barThickness: 24
+    //       }],
+    //       xAxes: [{
+    //         ticks: {
+    //           display: false,
+    //           beginAtZero: true
+    //         },
+    //         gridLines: {
+    //           display: false,
+    //           drawBorder: false
+    //         },
+    //       }],
+    //     },
+    //     legend: {
+    //       display: false
+    //     },
+    //     tooltips: {
+    //       enabled: false
+    //     }
+    //   }
+    // });
   }
 
   bind() {
